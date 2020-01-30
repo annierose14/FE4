93a82c4d7af07667fea89b1d805d9990e3fe1e63
annierose14 <60417200+annierose14@users.noreply.github.com>
2020-01-30 17:34:38 +0800

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -14,9 +14,9 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::any('exercise-1', 'Exercise1');
+Route::any('annierose-1', 'Exercise1');
 Route::any('annierose-2', 'Exercise2');
-Route::any('exercise-3', 'Exercise3@index');
-Route::any('exercise-3/store', 'Exercise3@store');
-Route::any('exercise-final', 'ExerciseFinal@login');
-Route::any('exercise-final/store', 'ExerciseFinal@store');
+Route::any('annierose-3', 'Exercise3@index');
+Route::any('annierose-3/store', 'Exercise3@store');
+Route::any('annierose-final', 'ExerciseFinal@login');
+Route::any('annierose-final/store', 'ExerciseFinal@store');
